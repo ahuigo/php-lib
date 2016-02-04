50213ad24a66c59d76997073a2a560630712c843
hilojack <a132811@gmail.com>
2016-02-04 15:48:53 +0800

    excel2csv

--- git/gitclean.sh
+++ git/gitclean.sh
@@ -1,5 +1,6 @@
 # This script is used to clean all git commit
 # Refer to : http://stackoverflow.com/questions/13716658/how-to-delete-all-commit-history-in-github
+# To delete sensitive data from git: https://help.github.com/articles/remove-sensitive-data/
 git checkout --orphan latest_branch
 git add -A
 git commit -am "Delete all previous commit"
