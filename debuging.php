2df7eab7b454d7c13c3ff6af7bcc215d78b0307a
hilojack <a132811@gmail.com>
2016-07-29 13:39:32 +0800

    debug

--- debuging.php
+++ debuging.php
@@ -1,10 +1,10 @@
 <?php
-isset($_GET['DEBUG']) && Debug::start();
+//isset($_GET['DEBUG']) && Debug::start();
 
 /**
  * DEBUG
  */
-class Debug{
+class Lib_Debug{
     /**
      * debuging('dtrace');          //查看调用栈
      * debuging($var);              //打印$var(var_dump)
@@ -26,8 +26,8 @@ class Debug{
 
         $force && $_GET['debug'] = 1;
         if(isset($_GET['debug'])){
-    		echo "================================= <br>\n";
-    		self::debugingPos();
+            echo "================================= <br>\n";
+            self::debugingPos();
             if($die === 2){
                 header('Content-type: application/json');
                 echo json_encode($var);
@@ -46,35 +46,38 @@ class Debug{
         }
     }
     static function debugingPos(){
-    	//$tmp = debug_backtrace(2, 2)[1];
-    	$tmp = debug_backtrace();
-    	$pos = $tmp[1];
+        //$tmp = debug_backtrace(2, 2)[1];
+        $tmp = debug_backtrace();
+        $pos = $tmp[1];
+        if(isset($pos['file'])){
+            echo "{$pos['file']} . (line:{$pos['line']})<br>\n";
+        }
         if(isset($pos['class'])){
             echo "{$pos['class']}->{$pos['function']} <br>\n";
         }else{
-            echo "{$pos['function']}(): {$pos['file']} . (line:{$pos['line']})<br>\n";
+            echo "{$pos['function']}(): <br>\n";
         }
     }
 
-	static $start_time = null;
-	static function start(){
-		self::$start_time = microtime(true);
-		if(function_exists('xhprof_enable')){
-			xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
-		}
-		ob_start(function($buf){
-			return self::getMsg() . $buf;
-		});
-	}
+    static $start_time = null;
+    static function start(){
+        self::$start_time = microtime(true);
+        if(function_exists('xhprof_enable')){
+            xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+        }
+        ob_start(function($buf){
+            return self::getMsg() . $buf;
+        });
+    }
 
-	static function getMsg(){
-		$msg = self::$CSS;
-		$msg .= self::getExtraMsg();
-		return $msg;
-	}
+    static function getMsg(){
+        $msg = self::$CSS;
+        $msg .= self::getExtraMsg();
+        return $msg;
+    }
 
     static private function getExtraMsg() {
-      $extraMsg = array(
+        $extraMsg = array(
             array('key', 'value'),
             array("memory_get_peak_usage: ", memory_get_peak_usage()/1000000 . " Mb"),
             array("exec_time", microtime(true) - self::$start_time." s"),
@@ -88,20 +91,20 @@ class Debug{
         if(function_exists('xhprof_disable')){
             $xhprof_data = xhprof_disable();
             $XHPROF_ROOT = '/opt/xhprof';
-			if(file_exists($XHPROF_ROOT)){
-				include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
-				include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
-				$source = 'xhprof_debug';
-        $xhprof_run =  (new XHProfRuns_Default());
-				$run_id = $xhprof_run->save_run($xhprof_data, $source);
-				$url = "http://{$_SERVER['SERVER_ADDR']}:9876/index.php?run={$run_id}&source=$source";
-				return "<a href='$url'> $url</a>";
-			}else{
-        return "no $XHPROF_ROOT";
-      }
-    }else{
-      return "no xhprof_disable";
-    }
+            if(file_exists($XHPROF_ROOT)){
+                include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+                include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+                $source = 'xhprof_debug';
+                $xhprof_run =  (new XHProfRuns_Default());
+                $run_id = $xhprof_run->save_run($xhprof_data, $source);
+                $url = "http://{$_SERVER['SERVER_ADDR']}:9876/index.php?run={$run_id}&source=$source";
+                return "<a href='$url'> $url</a>";
+            }else{
+                return "no $XHPROF_ROOT";
+            }
+        }else{
+            return "no xhprof_disable";
+        }
     }
 
     static private function toTable($arr) {
@@ -120,23 +123,23 @@ class Debug{
         $table .= '</table>';
         return $table;
     }
-	static function getTrace(){
-		try{
-			static $lastTime;
-			$lastTime or $lastTime = $_SERVER['REQUEST_TIME'];
-			$currTime = microtime(true);
-			$totalTime = $currTime-$_SERVER['REQUEST_TIME'];
-			$execTime = $currTime-$lastTime;$lastTime = $currTime;
-			$msg = "<hr>\n".
-				"execTime: $execTime s.<br/>\n" .
-				"totalTime: $totalTime s.<br/>\n".
-				"Trace:<br/>\n";
-			throw new Exception();
-		}catch(Exception $e){
-			//debug_print_backtrace();
-			return $msg . $e->getTraceAsString();
-		}
-		/*
+    static function getTrace(){
+        try{
+            static $lastTime;
+            $lastTime or $lastTime = $_SERVER['REQUEST_TIME'];
+            $currTime = microtime(true);
+            $totalTime = $currTime-$_SERVER['REQUEST_TIME'];
+            $execTime = $currTime-$lastTime;$lastTime = $currTime;
+            $msg = "<hr>\n".
+                "execTime: $execTime s.<br/>\n" .
+                "totalTime: $totalTime s.<br/>\n".
+                "Trace:<br/>\n";
+            throw new Exception();
+        }catch(Exception $e){
+            //debug_print_backtrace();
+            return $msg . $e->getTraceAsString();
+        }
+        /*
         static $lastTime;
         $lastTime or $lastTime = $_SERVER['REQUEST_TIME'];
         $currTime = microtime(true);
@@ -150,8 +153,8 @@ class Debug{
         $tmp = debug_backtrace();
         $msg .= var_export($tmp);
         return $msg;
-		 */
-	}
+         */
+    }
 
     static $CSS = <<<MM
     <style>
@@ -167,3 +170,4 @@ class Debug{
     </style>
 MM;
 }
+
