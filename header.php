b4dabae3b18812afd4e21b6bcdd7e9fbec6fe522
hilojack <a132811@gmail.com>
2017-10-12 16:18:35 +0800

    header

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+#$fp = fopen('php://input', 'r');
+#stream_set_blocking($fp, false);
+$a = [
+    'get'=>$_GET,
+    'post'=>$_POST,
+    'cookie'=>$_COOKIE,
+    'files'=>$_FILES,
+    'input'=>file_get_contents("php://input"),
+    'server'=>$_SERVER,
+    'headers'=>getallheaders(),
+];
+$count = $_COOKIE['count'] ?:0;
+setcookie('count', ++$count, time()+3600);
+var_dump($a);
