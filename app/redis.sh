6804004333481982fc199ab09598f17d57f98210
ahui132 <a132811@gmail.com>
2018-03-30 14:35:37 +0800

    del

--- app/redis.sh
+++
@@ -1,8 +0,0 @@
-cd ~
-set -o errexit;
-wget http://download.redis.io/releases/redis-3.2.3.tar.gz -O - | tar xzvf -
-cd redis-3.2.3
-make
-#export PATH=$PATH:./src/
-#make install
-nohup src/redis-server &
