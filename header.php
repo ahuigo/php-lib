f533e98c6b4f66cf84df6878ed1ba961eeae881c
ahuigo <ahuigo@ahuigo.com>
2020-04-08 22:00:03 +0800

    count

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <?php
 #$fp = fopen('php://input', 'r');
 #stream_set_blocking($fp, false);
+$count = $_COOKIE['count'] ??0;
+setcookie('count', ++$count, time()+3600);
 $a = [
+    'cookie'=>$count,
     'get'=>$_GET,
     'post'=>$_POST,
-    'cookie'=>$_COOKIE,
     'files'=>$_FILES,
     'input'=>file_get_contents("php://input"),
     'server'=>$_SERVER,
     'headers'=>getallheaders(),
 ];
-$count = $_COOKIE['count'] ??0;
-setcookie('count', ++$count, time()+3600);
 var_dump($a);
